01a3478cd6114c62fb657982547ea844b50ced2f
a4anddev <a4anddev@gmail.com>
2018-06-13 23:30:04 +0530

    add Fragment by xml i used same method as we use for create new activity

--- app/src/main/java/deeplearning/deeplearning/MainActivity.java
+++ app/src/main/java/deeplearning/deeplearning/MainActivity.java
@@ -6,28 +6,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity {
 
-    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn = findViewById(R.id.btnlogin);
-        btn.setOnClickListener(this);
-
-    }
-
-    /**
-     * Called when a view has been clicked.
-     *
-     * @param v The view that was clicked.
-     */
-    @Override
-    public void onClick(View v) {
-        String message = getString(R.string.hello_world);
-
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
