21ca7c7279064853ec845f3d202ea256fb748104
a4anddev <a4anddev@gmail.com>
2018-06-13 23:14:55 +0530

    lister set with implements class

--- app/src/main/java/deeplearning/deeplearning/MainActivity.java
+++ app/src/main/java/deeplearning/deeplearning/MainActivity.java
@@ -2,17 +2,30 @@ package deeplearning.deeplearning;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // get value form string file/ get anything from resouce folder
-        // change android to project get all files click to app ->build ->>source -> r ->debug -> select package -> r file there everything find r.layout/string/color everything
+        btn = findViewById(R.id.btnlogin);
+        btn.setOnClickListener(this);
+
+    }
+
+    /**
+     * Called when a view has been clicked.
+     *
+     * @param v The view that was clicked.
+     */
+    @Override
+    public void onClick(View v) {
         String message = getString(R.string.hello_world);
 
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
