0d8aaee575a898cffa1aa4e1367fcd8d77df0fe7
a4anddev <a4anddev@gmail.com>
2018-06-13 18:14:49 +0530

    view path r.class file and get string xml value with java

--- app/src/main/java/deeplearning/deeplearning/MainActivity.java
+++ app/src/main/java/deeplearning/deeplearning/MainActivity.java
@@ -2,6 +2,7 @@ package deeplearning.deeplearning;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // get value form string file/ get anything from resouce folder
+        // change android to project get all files click to app ->build ->>source -> r ->debug -> select package -> r file there everything find r.layout/string/color everything
+        String message = getString(R.string.hello_world);
+
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
