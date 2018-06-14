0c7775a0810de1e6f8924cff1a7513e714de6972
a4anddev <a4anddev@gmail.com>
2018-06-14 07:11:02 +0530

    Fragment attact with java code second method

--- app/src/main/java/deeplearning/deeplearning/MainActivity.java
+++ app/src/main/java/deeplearning/deeplearning/MainActivity.java
@@ -1,5 +1,8 @@
 package deeplearning.deeplearning;
 
+import android.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,6 +15,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentA obja = new FragmentA();
+
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.add(R.id.root, obja, "a");
+        transaction.commit();
+
+
 
     }
 }
